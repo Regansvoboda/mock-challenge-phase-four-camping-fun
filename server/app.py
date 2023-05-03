0affecf4ba642db3d7c06055a1fb689ed6135d03
Regan Svoboda <regansvoboda@gmail.com>
2023-05-02 20:29:38 -0500

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 from models import db, Camper, Activity, Signup
 
@@ -14,18 +15,89 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# api = Api(app)
+api = Api(app)
+
+class Campers(Resource):
+    def get(self):
+        campers_list = []
+        for c in Camper.query.all():
+            c_dict = {
+                'id': c.id,
+                'name': c.name,
+                'age': c.age
+            }
+            campers_list.append(c_dict)
+        return make_response(campers_list, 200)
+    def post(self):
+        data = request.get_json()
+        ca = Camper( name = data['name'], 
+                    age = data['age']
+                               )
+        try:
+            db.session.add( ca )
+            db.session.commit()
+            return make_response( ca.to_dict(), 201 )
+        except Exception as ex:
+            return make_response({
+                'error': ['validation error']
+            }, 422)
+    
+api.add_resource(Campers, '/campers')
+
+class CampersById(Resource):
+    def get(self, id):
+        c = Camper.query.filter_by(id = id).first()
+        if c == None:
+            return make_response({'error': 'Camper not found'}, 404)
+        return make_response(c.to_dict(), 200)
+    
+api.add_resource(CampersById, '/campers/<int:id>')
+
+class Activities(Resource):
+    def get(self):
+        a_list = []
+        for a in Activity.query.all():
+            a_dict = {
+                'id': a.id,
+                'name': a.name,
+                'difficulty': a.difficulty
+            }
+            a_list.append(a_dict)
+        return make_response(a_list, 200)
+
+api.add_resource(Activities, '/activities')
+
+class ActivitiesById(Resource):
+    def delete(self, id):
+        a_instance = Activity.query.filter_by(id = id).first()
+        if a_instance == None:
+            return make_response({'error': 'activity not found'}, 404)
+        db.session.delete(a_instance)
+        db.session.commit()
+        return make_response('', 204)
+
+api.add_resource(ActivitiesById, '/activities/<int:id>')
+
+class Signups(Resource):
+    def post(self):
+        data = request.get_json()
+        signups = Signup( camper_id = data['camper_id'], 
+                    activity_id = data['activity_id'],
+                    time = data['time']
+                               )
+        try:
+            db.session.add( signups )
+            db.session.commit()
+            return make_response(signups.activity.to_dict(), 201 )
+        except Exception as ex:
+            return make_response({
+                'error': [ex.__str__()]
+            }, 422)
+
+api.add_resource( Signups, '/signups' )
 
 
-@app.route('/')
-def index():
-    response = make_response(
-        {
-            "message": "Hello Campers!"
-        },
-        200
-    )
-    return response
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -6,19 +6,56 @@ from sqlalchemy.ext.associationproxy import association_proxy
 db = SQLAlchemy()
 
 
-class Camper(db.Model, SerializerMixin):
-    __tablename__ = 'campers'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
+    serialize_rules = ('-signups',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    difficulty = db.Column(db.Integer)
+
+    signups = db.relationship('Signup', backref='activity', cascade="all,delete, delete-orphan")
+    campers = association_proxy('signups', 'camper')
 
 
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
+    
+    # serialize_rules = ('-camper.signups', '-activities.signups',
+    #                    '-camper.activities', '-activity.campers')
 
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+
+
+    # @validates('time')
+    # def validates_year(self, key, time_int):
+    #     if 0> time >23:
+    #         raise ValueError('must have an time between 8 and 18')
+    #     return time_int
+
+
+class Camper(db.Model, SerializerMixin):
+    __tablename__ = 'campers'
+
+    serialize_rules = ('-signups', 'activities')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+
+    signups = db.relationship('Signup', backref='camper')
+    activities = association_proxy('signups', 'activity')
+
+
+    @validates('age')
+    def validates_age(self, key, age):
+        if age < 8:
+            raise ValueError("Camper too young.")
+        elif age > 18:
+            raise ValueError("Camper too old.")
+        return age
